f184916b9acd2af519207d718b4ba4d7161a0719
Hok Wui Wong <sw@Hoks-MacBook-Pro.local>
2020-04-01 02:13:33 +0800

    added page loader

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
   $('.header').height($(window).height());
 })
+
+document.onreadystatechange = function() { 
+  if (document.readyState !== "complete") { 
+    document.querySelector( 
+    "body").style.visibility = "hidden"; 
+    document.querySelector( 
+        "#loader").style.visibility = "visible"; 
+    } else { 
+        document.querySelector( 
+            "#loader").style.display = "none"; 
+            document.querySelector( 
+            "body").style.visibility = "visible"; 
+        } 
+}; 
